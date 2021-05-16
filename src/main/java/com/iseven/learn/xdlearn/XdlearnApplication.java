0c0330cc1d3179d6959a4bb54ba16f5ac038648a
cmyang <969312613@qq.com>
2021-05-16 20:38:25 +0800

    初始化项目

---
+++ src/main/java/com/iseven/learn/xdlearn/XdlearnApplication.java
@@ -0,0 +1,13 @@
+package com.iseven.learn.xdlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdlearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iseven/learn/xdlearn/XdlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iseven.learn.xdlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
